96218bb7845f5acf969e13232df5254a3f5d5066
Henrik S. Gaßmann <henrik@gassmann.onl>
2025-07-20 12:45:38 +0000

    build: initial integration of BLAKE2 sources

--- sources.cmake
+++ sources.cmake
@@ -21,6 +21,65 @@ file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/generated/src/dplx/blake2/detai
 configure_file(tools/config.hpp.in "${CMAKE_CURRENT_BINARY_DIR}/generated/src/dplx/blake2/detail/config.hpp" @ONLY)
 target_sources(libb2-reforged PUBLIC $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/generated/src/dplx/blake2/detail/config.hpp>)
 
+target_sources(libb2-reforged
+    PRIVATE
+        src/blake2.h
+        src/dplx/blake2.h
+        src/dplx/blake2/detail/blake2-impl.h
+        src/dplx/blake2/detail/blake2b-common.c.inc
+        src/dplx/blake2/detail/blake2s-common.c.inc
+        src/dplx/blake2/detail/blake2xb-generic.c
+        src/dplx/blake2/detail/blake2xs-generic.c
+)
+if ("SSE2" IN_LIST ACTIVE_IMPLEMENTATIONS
+    OR "SSE41" IN_LIST ACTIVE_IMPLEMENTATIONS
+    OR "AVX" IN_LIST ACTIVE_IMPLEMENTATIONS)
+
+    target_sources(libb2-reforged
+        PRIVATE
+            src/dplx/blake2/detail/blake2-x86-config.h
+            src/dplx/blake2/detail/blake2b-x86-round.h
+            src/dplx/blake2/detail/blake2s-x86-round.h
+
+            src/dplx/blake2/detail/blake2b-sse2-load.h
+            src/dplx/blake2/detail/blake2b-sse41-load.h
+            src/dplx/blake2/detail/blake2s-sse2-load.h
+            src/dplx/blake2/detail/blake2s-sse41-load.h
+    )
+endif()
+if ("NEON" IN_LIST ACTIVE_IMPLEMENTATIONS)
+
+    target_sources(libb2-reforged
+        PRIVATE
+            src/dplx/blake2/detail/blake2b-neon-load.h
+            src/dplx/blake2/detail/blake2b-neon-round.h
+            src/dplx/blake2/detail/blake2s-neon-load.h
+            src/dplx/blake2/detail/blake2s-neon-round.h
+    )
+endif()
+foreach(IMPL IMPL_FILE_PART IN ZIP_LISTS ACTIVE_IMPLEMENTATIONS ACTIVE_IMPLEMENTATION_FILE_PARTS)
+    target_sources(libb2-reforged
+        PRIVATE
+            src/dplx/blake2/detail/blake2b-${IMPL_FILE_PART}.c
+            src/dplx/blake2/detail/blake2s-${IMPL_FILE_PART}.c
+    )
+    set_source_files_properties(
+        src/dplx/blake2/detail/blake2b-${IMPL_FILE_PART}.c
+        src/dplx/blake2/detail/blake2s-${IMPL_FILE_PART}.c
+
+        PROPERTIES
+            COMPILE_FLAGS "${DPLX_BLAKE2_CFLAGS_${IMPL}}"
+            COMPILE_DEFINITIONS "DPLX_BLAKE2_IMPL_NAME=${IMPL_FILE_PART};DPLX_BLAKE2_NO_DISPATCH=$<BOOL:${DPLX_BLAKE2_NO_DISPATCH}>"
+    )
+endforeach()
+
+if (DPLX_BLAKE2_WITH_LIBB2_COMPAT)
+    target_sources(libb2-reforged
+        PRIVATE
+            src/dplx/blake2/detail/libb2-compat.c
+    )
+endif()
+
 if (BUILD_TESTING)
     dplx_target_sources(libb2-reforged-tests PRIVATE
         MODE VERBATIM
